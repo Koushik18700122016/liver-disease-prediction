046271f1cbe57c5dffc2dd84076cec580eedee6e
Koushik <koushikdasmid2019@gmail.com>
2026-04-24 12:36:07 +0530

    Initial commit - Liver Disease ML Web App

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,6 @@
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 export const predict = async (data) => {
-  const res = await fetch("http://127.0.0.1:8000/predict", {
+  const res = await fetch(`${BASE_URL}/predict`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
